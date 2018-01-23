fe4a69c7c4fb93c1bf46c2c11cf87fdad2fde72c
Gaëlle Calmels <calmels.gaelle@gmail.com>
2018-01-23 16:59:06 +0100

    add of Film folder

---
+++ src/containers/Film/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import './style.css'
+
+class Film extends Component {
+  render() {
+    return (
+      <div className="film">
+        This is the Film page
+      </div>
+    )}
+}
+
+export default Film
