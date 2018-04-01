388021b1690e08f79faa7c02182ac83a19b8b3ce
Estelle Guingo <estelle.guingo@gmail.com>
2018-04-01 16:55:45 +0200

    Project and Team Css adjustements

--- src/containers/Team/index.js
+++ src/containers/Team/index.js
@@ -12,34 +12,37 @@ import { Carousel } from 'react-responsive-carousel';
 const members = data
 class Team extends Component {
   render() {
-    const memberCarouselFirst = <div className = "slide-team-members"><MemberSheet member={members[0]}/><MemberSheet member={members[1]}/></div>
-    const memberCarouselSecond = <div className = "slide-team-members"><MemberSheet member={members[2]}/><MemberSheet member={members[3]}/></div>
+    const memberCarouselFirst = <div className="slide-team-members"><MemberSheet member={members[0]} /><MemberSheet member={members[1]} /></div>
+    const memberCarouselSecond = <div className="slide-team-members"><MemberSheet member={members[2]} /><MemberSheet member={members[3]} /></div>
 
     return (
       <div className="team">
-        <Carousel
-          showStatus ={false}
-          showIndicators={false}
-          showThumbs={false}
-          width={"100%"}
-          infiniteLoop={true}
+        <div className="team-row">
+          <Carousel
+            showStatus={false}
+            showIndicators={false}
+            showThumbs={false}
+            width={"100%"}
+            infiniteLoop={true}
           >
             {memberCarouselFirst}
             {memberCarouselSecond}
-        </Carousel>
-        <div className = "project-button">
-          <ImageButton 
-            className = "project-image-button"
+          </Carousel>
+        </div>
+        <div className="project-button">
+          <ImageButton
+            className="project-image-button"
             onClick={() => this.props.history.push('/project')}
             src="./assets/images/logo.svg"
             alt="logo humanium"
           />
-        <a className="project-button-text" onClick={() => this.props.history.push('/project')}>
-          Project
+          <a className="project-button-text" onClick={() => this.props.history.push('/project')}>
+            Project
         </a>
         </div>
       </div>
-    )}
+    )
+  }
 }
 
 export default withRouter(Team)
