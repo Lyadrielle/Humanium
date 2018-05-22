2aa250bdbcc7ac32a770f4557641f6d6212f5c44
Estelle Guingo <estelle.guingo@gmail.com>
2018-05-23 00:22:48 +0200

    clear showmodal function

--- src/components/TreeButton/index.js
+++ src/components/TreeButton/index.js
@@ -8,7 +8,7 @@ class TreeButton extends Component {
     return (
       <div>
         <div data-tip={this.props.node.name} className="border" onClick={() => {
-          this.props.onNodeSelect(this.props.index)
+          this.props.onNodeSelect(this.props.node)
         }}>
           <div className="node-name">{this.props.node.title}</div>
           <div className="losange"></div>
